97b855c3e5d9a2f639ab1448893876892bb773ac
Younes Benmoussa <younes-benmoussa@pm.me>
2025-08-19 15:28:52 +0100

    update pkg install for servers and deleted dekstop pkg install

--- system-files/pkg-install.sh
+++ system-files/pkg-install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+set -e
+
 DEBIAN_FRONTEND=noninteractive
  
-
 #install packages
-sudo apt update && sudo apt upgrade -y
+sudo apt update 
+sudo apt upgrade -y
 
 sudo apt install -y --no-install-recommends \
   net-tools rsync wget curl dnsutils apt-transport-https ca-certificates git htop linux-headers-amd64 \
   clang clangd clang-tidy clang-tools clang-format \
   build-essential make meson ninja-build bison gnupg  \
-  pkg-config cmake make pbzip2 mdadm texinfo ufw fail2ban unzip
+  pkg-config cmake pbzip2 mdadm texinfo ufw fail2ban unzip \
+  nfs_common systemd-resolved system-timesyncd
